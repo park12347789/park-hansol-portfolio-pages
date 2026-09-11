d113dcad9699a9d0fcaac1f6805358f2c6f8c8a3
park12347789 <67284700+park12347789@users.noreply.github.com>
2026-09-12 01:23:00 +0900

    feat: enable Last Convoy gameplay overhaul

--- last-convoy-evac-zone/game.js
+++ last-convoy-evac-zone/game.js
@@ -1,4 +1,4 @@
 import * as THREE from 'three';
-const files=['game.part1.js','game.part2a.js','game.part2b.js','game.part3a.js','game.part3b.js','game.part4a.js','game.part4b.js'];
+const files=['game.part1.js','game.part2a.js','game.part2b.js','game.part3a.js','game.part3b.js','game.part4a.js','game.part4b.js','game.part5a.js','game.part5b.js','game.part5c.js'];
 const parts=await Promise.all(files.map(f=>fetch('./'+f).then(r=>{if(!r.ok) throw new Error(f+' '+r.status); return r.text();})));
 new Function('THREE', parts.join('\n'))(THREE);
